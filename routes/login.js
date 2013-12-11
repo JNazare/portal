89a1eabeba5047d9639fadd7da8c02ccc10ec786
Juliana Nazare <jules.nazare@gmail.com>
2013-12-11 17:57:02 -0500

    push to sandbox

--- routes/login.js
+++ routes/login.js
@@ -7,7 +7,7 @@ exports.get_oauth_code = function(req, res){
 	if(req.method =='GET') {
        var pathName = "https://github.com/login/oauth/authorize";
        var client_id = process.env.CLIENT_ID;
-       var redirect_uri = "http://portal-sandbox.herokuapp.com/home";
+       var redirect_uri = "http://localhost:3000/home";
        var scope = "repo";
        res.redirect(pathName+"?client_id="+client_id+"&scope="+scope+"&redirect_uri="+redirect_uri);
 	}
@@ -17,7 +17,7 @@ exports.get_oauth_token = function(req, res){
 	var code = req.url.split("=")[1]
 	var pathName = "https://github.com/login/oauth/access_token";
   	var client_id = process.env.CLIENT_ID;
-  	var redirect_uri = "http://portal-sandbox.herokuapp.com/home";
+  	var redirect_uri = "http://localhost:3000/home";
   	var client_secret = process.env.CLIENT_SECRET;
   	var fullPath = pathName+"?client_id="+client_id+"&redirect_uri="+redirect_uri+"&client_secret="+client_secret+"&code="+code;
   	request(fullPath, function (error, response, token) {
