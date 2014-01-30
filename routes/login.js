c0111940d22c16756055734c23d45c6605a280c7
Juliana Nazare <jules.nazare@gmail.com>
2014-01-30 01:55:49 -0500

    clean up

--- routes/login.js
+++ routes/login.js
@@ -25,7 +25,7 @@ exports.get_oauth_token = function(req, res){
     	helpers.get_tree(req, function(tree){ 
 			helpers.get_user(req, function(user_info){
 				req.session.user_info = user_info;
-				res.render('layout2', {
+				res.render('home', {
 					copy: req.session.copy,
 					user: req.user,
     				tree: tree,
@@ -55,7 +55,7 @@ exports.get_oauth_token = function(req, res){
 	    			helpers.get_user(req, function(user_info){
 	    				req.session.user_info = user_info;
 	    				console.log(user_info);
-	    				res.render('layout2', {
+	    				res.render('home', {
 	    					copy: req.user.city,
 	    					user: req.user,
 		    				tree: tree,
